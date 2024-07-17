8addcd1e88aeec78b113526abeaecb3bfadd7cfe
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-07-17 16:31:42 +0100

    enable page scroll on SongOutline

--- src/components/Songs/Song/SongOutline/SongOutline.vue
+++ src/components/Songs/Song/SongOutline/SongOutline.vue
@@ -2,13 +2,21 @@
 
 const props = defineProps(['song'])
 
+
+const go_section = daw => {
+   console.log('go_section',daw)
+   let target = document.getElementById(daw)
+   if(target !== null) target.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"})
+}
+
 </script>
 
 <template>
    <div v-if="song">
       <div class="flex gap-1 m_1">
-         <div v-for="section in song.songsheet.aSections" :key="section.id" v-on:click="go_to_section(section.id)"
-            class="bordered p_0.5 py_0.5">
+         <div v-for="section in song.songsheet.aSections" :key="section.id"
+            class="bordered p_0.5 py_0.5 cursor_pointer" 
+            @click="go_section(section.daw)" >
             {{ section.daw }} {{ section.title }}
          </div>
       </div>
