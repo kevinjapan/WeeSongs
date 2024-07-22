c9c1325e1cc9f8016f4d0a3c6734eb53ce359adb
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-07-22 12:57:17 +0100

    fix AppStatus

--- src/components/App/AppStatus/AppStatus.vue
+++ src/components/App/AppStatus/AppStatus.vue
@@ -1,54 +1,35 @@
 <script setup>
 import { ref, watch } from 'vue'
 
-// use Component v-model - v-model can be used on a component to implement a two-way binding
-// to do : research - naming 'model' ? multiple 'models' ?
-
-const model = defineModel()
-
 
 // AppStatus 
 // app-level flash notification utility component
 // we don't use v-if in parent component, since we want to use transitions (v-if just toggles display)
-// wrapper around AppStatusPopup to allow us to empty the notification message and transition
 
-// to do : text still disappears just before transition completes - awkward
+// we use Component v-model to implement two-way binding
+// empties the parent components notify_msg w/out any explicit call
+const notification_msg = defineModel()
 
-const props = defineProps(['message'])
+// since msg is our toggle flag, transitions take effect after the msg is emptied,
+// we linger the text to allow transition effects to act on the text-filled component
+const lingering_text = ref('')
 
-// create local prop we can empty after timeout
-const status_msg = ref('');
-
-// linger to allow transition effect on text
-const linger_status_msg = ref(model)
-watch(model, () => {
-      setTimeout(() => {model.value = ''},3000)
+// assign msg then setTimeout to clear
+watch(notification_msg, () => {
+   if(notification_msg.value !== '') lingering_text.value = notification_msg.value
+   setTimeout(() => {notification_msg.value = ''},3000)
 })
-
-// watch prop.messages - get local ref using toRef()
-// this method works but does not empty the parent's prop
-// future : review Component v-model - recommended method for this type of binding
-// const prop_status_msg = toRef(props,'message')
-// watch(prop_status_msg,  () => {
-//    status_msg.value = prop_status_msg.value
-//    linger_status_msg.value = prop_status_msg.value
-//    setTimeout(() => {status_msg.value = ''},3000)
-// })
 </script>
 
 <template>
-   <div class="app_status" :class="{app_status_bg: model !== ''}">{{ linger_status_msg }}
+   <div class="app_status" :class="{app_status_bg: notification_msg !== ''}">
+      {{ lingering_text }}
    </div>
 </template>
 
 <style scoped>
- 
-/* 
-    AppStatus
-    rqrd behavioural styling here - 'styling only' is w/ tailwind in component
-*/
 .app_status {
-
+   
    position:fixed;
    top:var(--status-msg-top);
    left:0;
@@ -66,7 +47,6 @@ watch(model, () => {
    -webkit-transform: translateY(-200%);
    -ms-transform: translateY(-200%);
    transform: translateY(-200%);
-
    opacity:0;
 
    -webkit-transition:opacity .35s ease-in-out,-webkit-transform .75s ease-in-out;
@@ -75,13 +55,13 @@ watch(model, () => {
    transition:opacity .35s ease-in-out,transform .75s ease-in-out;
    transition:opacity .35s ease-in-out,transform .75s ease-in-out,-webkit-transform .75s ease-in-out;
 }
+
 .app_status_bg {
 
    /* transitions */
    -webkit-transform: translateY(0);
    -ms-transform: translateY(0);
    transform: translateY(0);
-
    opacity:1;
 
    -webkit-transition:opacity 1s ease-in-out,-webkit-transform .75s ease-in-out;
