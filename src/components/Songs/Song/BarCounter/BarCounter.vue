8ac880d5ada076bc273c773d516d908e3a1a4518
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-07-24 14:44:07 +0100

    simplify and fix emit and handling

--- src/components/Songs/Song/BarCounter/BarCounter.vue
+++ src/components/Songs/Song/BarCounter/BarCounter.vue
@@ -9,7 +9,7 @@ const local_num_bars = ref(props.num_bars)
 
 // although not clear from docs, defining emits makes it function much more easily,
 // otherwise have a lot of probs getting this working w/ converting case btwn emit and @.
-// const emit = defineEmits(['barNumsChanged'])
+// const emit = defineEmits(['bar-nums-changed'])
 
 // v-model below is equivalent to:
 // <input
@@ -21,7 +21,7 @@ const local_num_bars = ref(props.num_bars)
 <template>
    <input 
       v-model="local_num_bars"
-      @keyup.enter="$emit('barNumsChanged',local_num_bars)"
+      @keyup.enter="$emit('bar-nums-changed',local_num_bars)"
       class="w-12 text-slate-400" 
       name="num_bars"  
       type="text"
--- src/components/Songs/Song/SongSection/SongSectionTitles/SongSectionTitles.vue
+++ src/components/Songs/Song/SongSection/SongSectionTitles/SongSectionTitles.vue
@@ -1,7 +1,7 @@
 <script setup>
 import BarCounter from '../../BarCounter/BarCounter.vue'
 
-const emit = defineEmits(['barNumsChanged'])
+const emit = defineEmits(['bar-nums-changed'])
 const props = defineProps(['section','num_bars','notify_updated_titles','editable'])
 
 // to do : we are mutating child properties of 'section' prop   
@@ -10,7 +10,7 @@ const props = defineProps(['section','num_bars','notify_updated_titles','editabl
 const update = () => props.notify_updated_titles()
 
 const change_num_bars = (num) => {
-   emit('barNumsChanged',num)
+   emit('bar-nums-changed',num)
 }
 
 </script>
@@ -19,27 +19,31 @@ const change_num_bars = (num) => {
    <div style="display:flex;justify-content:start;max-width:500px;gap:0.5rem;" class="p_0.5">
       edit {{ props.editable }}
 
-      <input 
-         v-model="props.section.daw" 
-         class="w-12 text-slate-400" 
-         name="daw" 
-         style="width:15%;" 
+      <input
+         v-model="props.section.daw"
+         class="w-12 text-slate-400"
+         name="daw"
+         style="width:15%;"
          type="text"
          @input="update"
          :readonly="props.editable === false"
       />
 
-      <input 
-         v-model="props.section.title" 
-         class="w-32 text-slate-400" 
-         name="title" 
-         style="width:85%;" 
+      <input
+         v-model="props.section.title"
+         class="w-32 text-slate-400"
+         name="title"
+         style="width:85%;"
          type="text"
          @input="update"
          :readonly="props.editable === false"
       />
       
-      <BarCounter :num_bars="props.num_bars" @bar-nums-changed="change_num_bars" :editable="props.editable"></BarCounter>
+      <BarCounter
+         :num_bars="props.num_bars"
+         :editable="props.editable"
+         @bar-nums-changed="change_num_bars"
+      />
    
    </div>
 
