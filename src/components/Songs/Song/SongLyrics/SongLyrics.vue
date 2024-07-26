6999afe674e730053402254f2fae0fdb5578cb5d
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-07-26 14:35:38 +0100

    Update SongLyrics.vue

--- src/components/Songs/Song/SongLyrics/SongLyrics.vue
+++ src/components/Songs/Song/SongLyrics/SongLyrics.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 import { useSongStore } from '@/stores/songStore'
 import SongOutline from '../SongOutline/SongOutline.vue'
-import SongSection from '../SongSection/SongSection.vue'
+import SongSectionLyrics from './SongSectionLyrics/SongSectionLyrics.vue'
 
 
 // SongLyrics
@@ -11,13 +11,10 @@ const song_store = useSongStore()
 const props = defineProps(['song'])
 const notify_msg = ref('')
 
-
-
 const last = (index) => {
    return index === song_store.song.songsheet.aSections.length - 1
 }
 
-// to do : on newly created songs, fails on key below - since section.id is not set?
 
 </script>
 
@@ -25,18 +22,11 @@ const last = (index) => {
 
    <SongOutline :song="song" />
 
-
-   <SongSection v-for="(section, index) in song_store.song.songsheet.aSections" 
+   <SongSectionLyrics v-for="(section, index) in song_store.song.songsheet.aSections" 
       :key="section.id" 
-      :section="section" 
-      :update_song="props.update_song" 
+      :section="section"
       :index="index" 
       :last="last(index)"
-      :editable="false"
-      :del_section="props.del_section"
-      :clone_section="props.clone_section"
-      :move_section="props.move_section"
-      :update_section="props.update_section"
    />
   
    <AppStatus v-model="notify_msg" />
