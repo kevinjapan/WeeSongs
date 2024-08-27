5c1c22ec9b1d537b75741c8e870452ac6ca26885
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-08-27 18:57:02 +0100

    Create AlbumTracksList.vue

---
+++ src/components/Albums/AlbumTracksList/AlbumTracksList.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+
+const props = defineProps(['songs'])
+
+</script>
+
+<template>
+
+   <section>
+      <h3>Tracks</h3>
+      <ul>
+         
+         <li v-for="song in props.songs" :key="song.id">
+            {{ song.title }}
+         </li>
+
+      </ul>
+   </section>
+
+</template>
+
+
+<style scoped>
+ul {
+   text-align:left;
+}
+</style>
+
+
