c7dcb7c5472bcb4e65d112f9637830570bf79007
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-08-27 12:59:41 +0100

    Create newAlbumTemplate.js

---
+++ src/utilities/newAlbumTemplate/newAlbumTemplate.js
@@ -0,0 +1,13 @@
+
+export function get_new_album_template () {
+
+   return {
+      "title": "New Album",
+      "slug": "new-album",
+      "outline": "new album outline",
+      "deleted_at": null,
+      "songs": []
+
+  }
+
+}
