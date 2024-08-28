7e105bcc6ba43168dfa68b37da8fa8907a6fc734
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-08-28 16:24:59 +0100

    fix nav in lg screens (sm on-going)

--- src/components/App/AppNav/AppNav.vue
+++ src/components/App/AppNav/AppNav.vue
@@ -5,9 +5,9 @@ import { useAppStore } from '@/stores/appStore'
 const router = useRouter()
 const app_store = useAppStore()
 
-const close_app_nav = () => {
-   app_store.display_nav = false
-}
+// const close_app_nav = () => {
+//    app_store.display_nav = false
+// }
 
 // we use router.push() rather than:
 // <RouterLink to="/" class="view_link" activeClass="selected_view" exactActiveClass="selected_view">Home</RouterLink>
@@ -17,15 +17,20 @@ const open_nav_link = route => {
    router.push(route)
 }
 
+
+//  v-if="app_store.display_nav === true"
+
+
+// <button @click="app_store.display_nav = true" class="app_nav_hamburger">
+//             <img src="../../../assets/icons/list.svg" alt="open nav menu"/>
+//          </button>
 </script>
 
 
 <template>
    <Transition>
 
-      <nav v-if="app_store.display_nav === true" class="flex flex_col align_items_end app_nav">
-
-         <button @click="close_app_nav" class="close_app_nav mt_3 fit_content">X</button>
+      <nav class="app_nav flex flex_col align_items_end">
 
          <a @click="open_nav_link('/')">Home</a>
          <a @click="open_nav_link('/songs')">Songs</a>
@@ -39,10 +44,6 @@ const open_nav_link = route => {
 
       </nav>
 
-      <div v-else >
-         <button @click="app_store.display_nav = true" class="app_nav_hamburger">M E N U 2</button>
-      </div>
-
    </Transition>
 </template>
 
@@ -52,53 +53,60 @@ nav.app_nav {
    position:fixed;
    top:0;
    right:0;
-   z-index:90000;    /* to do : review all layering */
+   z-index:var(--nav_layer);
    text-align:right;
 
-   width:fit-content;   /* to do : set media query w/ 100% width for sm */
+   width:fit-content;
    height:fit-content;
-
-   padding:2rem;
-   font-size:1.5rem;
+   margin:0;
+   padding:0;
+   font-size:1.2rem;
    font-weight:300;
-   color:white;
-   background:blue;
-}
-/* configure Vue Transition component for app_nav slide-in*/
-.v-enter-active,.v-leave-active {
-   -webkit-transition: opacity .25s ease-in-out;  
-   -o-transition: opacity .25s ease-in-out;
-   transition: .25s ease-in-out;
-   opacity:1;
-   
-   -webkit-transform: translate(0, 0);
-   -ms-transform: translate(0, 0);
-   transform: translate(0, 0);
-}
-.v-enter-from,.v-leave-to {
-   -webkit-transform: translate(100%, 0);  
-   -ms-transform: translate(100%, 0);
-   transform: translate(100%, 0);
-   opacity:0;
 }
 
 nav.app_nav > div {
-   /* to do : review : div? */
    cursor:pointer;
    border:solid 1px lightgrey;
+}
+
+@media (min-width: 768px) {
+   
+   nav.app_nav {
+      position:fixed;
+      width:100%;
+      height:var(--app_nav_height);
+      padding-right:2rem;
+      overflow:hidden;
+
+      flex-direction:row;
+      justify-content:flex-end;
+      gap:1rem;
+      font-size:1.1rem;
+
+      background:white;
+   }
+
 }
 .app_nav_hamburger {
    position:fixed;
    top:0;
    right:0;
-   z-index:999999999999999;
-   width:100px;
+   z-index:var(--nav_layer);
+   width:fit-content;
    height:fit-content;
+   margin:1rem;
    background:white;
 }
+.app_nav_hamburger img {
+   width:32px;
+   height:32px;
+}
 a {
+   margin:0;
+   padding:0;
    color:inherit;
    cursor:pointer;
+   font-weight:400;
 }
 button {
    width:fit-content;
@@ -110,5 +118,28 @@ button {
 .close_app_nav {
    margin-left:auto;
    margin-right:0;
+}
+
+
+/* 
+   configure Vue Transition component for app_nav slide-in
+*/
+@media (min-width: 768px) {
+   .v-enter-active,.v-leave-active {
+      -webkit-transition: opacity .25s ease-in-out;  
+      -o-transition: opacity .25s ease-in-out;
+      transition: .25s ease-in-out;
+      opacity:1;
+      
+      -webkit-transform: translate(0, 0);
+      -ms-transform: translate(0, 0);
+      transform: translate(0, 0);
+   }
+   .v-enter-from,.v-leave-to {
+      -webkit-transform: translate(100%, 0);  
+      -ms-transform: translate(100%, 0);
+      transform: translate(100%, 0);
+      opacity:0;
+   }
 }
 </style>
