0e8820cd60b13ca3765c2d7aaa73565d98d7cfd5
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-09-05 22:52:31 +0100

    Create useData.js

---
+++ src/utilities/useData/useData.js
@@ -0,0 +1,27 @@
+import { useAppStore } from '@/stores/appStore'
+import useEndPoints from '../useEndPoints/useEndPoints'
+import useFetch from '../useFetch/useFetch'
+
+
+// useData composable
+
+// we want ability to change between static (no-server, data in files) and server-api served site
+// our application will call useData and herein we will map to correct end-point.
+// by using this middleman, we only have a single point of change to achieve this (eg components don't care what the url is)
+// future : we want to seamlessly interchange btwn static files and server-api served data sources.
+// since we want to build server-api served dev sites but demo as static
+// - in dev: const { data, error } = await useFetch(`${app_store.app_api}/songs?order_by=${order_by.value}&asc=${asc.value}&page=${page.value}`,reqInit())
+// - in static: const { data, error } = await useFetch(`./songs_list.json`,reqInit())
+
+export default async function useData(end_point,query_params,options) {
+
+   const end_points = useEndPoints()
+
+   if(!Object.hasOwn(end_points,end_point)) return false
+
+   const end_point_url = end_points[end_point]
+   const query_string = new URLSearchParams(query_params)
+   
+   return await useFetch(`${useAppStore().app_api}${end_point_url}?${query_string}`,options)
+}
+
