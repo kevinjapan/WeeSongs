5513822f9670fe1d56adab5178874bbc973ac1f2
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-09-11 16:56:30 +0100

    add arg comments

--- src/utilities/useData/useData.js
+++ src/utilities/useData/useData.js
@@ -8,12 +8,16 @@ import useFetch from '../useFetch/useFetch'
 // we want ability to change between static (no-server, data in files) and server-api served site
 // our application will call useData and herein we will map to correct end-point.
 // by using this middleman, we only have a single point of change to achieve this (eg components don't care what the url is)
-// future : we want to seamlessly interchange btwn static files and server-api served data sources.
+// future : 
+// we want to seamlessly interchange btwn static files and server-api served data sources.
 // since we want to build server-api served dev sites but demo as static
 // - in dev: const { data, error } = await useFetch(`${app_store.app_api}/songs?order_by=${order_by.value}&asc=${asc.value}&page=${page.value}`,reqInit())
 // - in static: const { data, error } = await useFetch(`./songs_list.json`,reqInit())
-//
-//
+
+
+// useData
+// @url_params  : array of url params (ids/slugs) to include in url path
+// @query_params: object containing key/value pairs to build query string
 
 export default async function useData(end_point,url_params,query_params,options) {
 
