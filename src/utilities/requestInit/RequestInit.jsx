6c3f79466a8e3d5bed8be1ba690b1b5d91ea0cad
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-09-16 15:02:29 +0100

    add bearer_token for DELETE etc

--- src/utilities/requestInit/RequestInit.jsx
+++ src/utilities/requestInit/RequestInit.jsx
@@ -2,28 +2,34 @@
 // set headers for fetch requests
 //
 export default function reqInit(method = "POST", bearer_token = "",body = "") {
-    if(typeof body === 'object') {
-        body = JSON.stringify(body)
-    }
-    if(body && body !== "" && body !== undefined) {
-        return {
-            method: method,
-            headers: { 
-                'Accept':'application/json',
-                'Authorization': `Bearer ${bearer_token}`,
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include',
-            mode: "cors",
-            cache: "default",
-            body: body
-        }
-    }
-    else {
-        return {
-            method: "GET",
-            mode: "cors",
-            cache: "default"
-        }
-    }
+
+   if(typeof body === 'object') {
+      body = JSON.stringify(body)
+   }
+
+   if(body && body !== "" && body !== undefined) {
+      return {
+         method: method,
+         headers: { 
+               'Accept':'application/json',
+               'Authorization': `Bearer ${bearer_token}`,
+               'Content-Type': 'application/json',
+         },
+         credentials: 'include',
+         mode: "cors",
+         cache: "default",
+         body: body
+      }
+   }
+   else {
+      return {
+         method: method,
+         headers: { 
+            'Authorization': `Bearer ${bearer_token}`,
+            'Accept':'application/json',
+         },
+         mode: "cors",
+         cache: "default"
+      }
+   }
 }
