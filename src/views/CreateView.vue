03e2559f2216a85c2966af53aaa5009be3cf1392
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-09-17 12:48:11 +0100

    enable create album

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,23 +1,22 @@
 <script setup>
 import { ref } from 'vue'
 import { useAppStore } from '@/stores/appStore'
+import { useAlbumStore } from '@/stores/albumStore'
 import { useSongStore } from '@/stores/songStore'
 
 
 // CreateView
 
 const app_store = useAppStore()
+const albumStore = useAlbumStore()
 const songStore = useSongStore()
-const title = ref('')
+
+const song_title = ref('')
+const album_title = ref('')
 
 
 const apply_song = async() => {
-
-   // to do : enable create song and create album
-   
-   console.log('apply song')
-   return
-   const result = await songStore.create_song(title.value)
+   const result = await songStore.create_song(song_title.value)
    if(result && result.outcome) {
       app_store.set_notify_msg_list(result.outcome === 'success' 
          ? 'The song was successfully created' 
@@ -25,7 +24,12 @@ const apply_song = async() => {
    }
 }
 const apply_album = async() => {
-   console.log('apply album')
+   const result = await albumStore.create_album(album_title.value)
+   if(result && result.outcome) {
+      app_store.set_notify_msg_list(result.outcome === 'success' 
+         ? 'The album was successfully created' 
+         : 'We were unable to create the album. ' + result.message)
+   }
 }
 
 </script>
@@ -37,29 +41,31 @@ const apply_album = async() => {
 
       <h3>Create new song</h3>
       <section class="grid form_grid">
-         <label>Title</label>
+         <label for="song_title">Title</label>
          <input 
-            v-model="title"
-            name="title" 
+            v-model="song_title"
+            id="song_title"
+            name="song_title" 
             className="border border-slate-200"
             placeholder="title here"
          />
          <div></div>      
-         <button type="submit" v-if="app_store.is_logged_in()">Apply</button>
+         <button type="submit" v-if="app_store.is_logged_in() && song_title.length > 0">Apply</button>
          <button type="button" v-else disabled>Apply</button>
       </section>
 
       <h3>Create new album</h3>
       <section class="grid form_grid">
-         <label>Title</label>
+         <label for="album_title">Title</label>
          <input 
-            v-model="title"
-            name="title" 
+            v-model="album_title"
+            id="album_title"
+            name="album_title" 
             className="border border-slate-200"
             placeholder="title here"
          />
          <div></div>      
-         <button type="button" v-if="app_store.is_logged_in()" @click="apply_album">Apply</button>
+         <button type="button" v-if="app_store.is_logged_in() && album_title.length > 0" @click="apply_album">Apply</button>
          <button type="button" v-else disabled>Apply</button>
       </section>
 
