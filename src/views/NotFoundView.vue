0cd03b8f1ce386291e3aa0a341fee46a556a67cd
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-09-19 12:30:02 +0100

    Create NotFoundView.vue

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+
+// NotFoundView
+
+const show = ref(false)
+
+onMounted(() => {
+   setTimeout(() => show.value = true,500)
+})
+
+</script>
+
+<template>
+
+   <section class="mt_5">
+      <Transition>
+            <h3 v-if="show" class="mt_5 p_3">Page Not Found</h3>
+      </Transition>  
+   </section>
+
+</template>
+
+
+<style scoped>
+.v-enter-active,.v-leave-active {
+   -webkit-transition: opacity .5s ease-in-out;  
+   -o-transition: opacity .5s ease-in-out;
+   transition: opacity .5s ease-in-out;
+}
+.v-enter-from,.v-leave-to {
+   opacity: 0;
+}
+</style>
+
