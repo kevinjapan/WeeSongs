e289fbcd3546bfbd046fa2321ed48674b08767ea
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-09-20 13:41:52 +0100

    disable nav on edge pages

--- src/components/PaginationNav/PaginationNav.vue
+++ src/components/PaginationNav/PaginationNav.vue
@@ -1,7 +1,8 @@
 <script setup>
+import { computed } from 'vue'
+
 
 // PaginationNav
-
 
 // Component Interface - props and emits
 const props = defineProps({
@@ -14,7 +15,14 @@ const emit = defineEmits(
 )
 
 // Component methods
+const computed_last_page = computed(() => {
+   return parseInt(props.page_links[props.page_links.length - 1].label)
+})
 const step = (step) => {
+   const last_page = computed_last_page
+   if(!isNaN(last_page)) {
+      if( parseInt(props.page) + step > last_page || parseInt(props.page) + step < 1 ) return
+   }
    emit('step-to-page',step)
 }
 const go_to_page = (selected_page) => {
@@ -23,16 +31,24 @@ const go_to_page = (selected_page) => {
 const is_selected_link = (link_label => {
    return parseInt(link_label) === parseInt(props.page)
 })
+
+const on_first_page = computed(() => {
+   return props.page === 1
+})
+const on_last_page = computed(() => {
+   return props.page === computed_last_page.value}
+)
 </script>
 
 
 <template>
    <ul class="nav_bar justify_between">
-      <li v-on:click="step(-1)" class="cursor_pointer italic">< prev</li>
+      <li v-on:click="step(-1)" class="cursor_pointer italic" :class="{greyed_out: on_first_page}">< prev</li>
       <li v-for="link in page_links" :key="link.label" v-on:click="go_to_page(link.label)" 
-         class="cursor_pointer" :class="{ selected_link: is_selected_link(link.label) }"
-         >{{ link.label }}</li>
-      <li v-on:click="step(1)" class="cursor_pointer italic">next ></li>
+         class="page_link cursor_pointer" :class="{selected_link: is_selected_link(link.label)}">
+         {{ link.label }}
+      </li>
+      <li v-on:click="step(1)" class="cursor_pointer italic" :class="{greyed_out: on_last_page}">next ></li>
    </ul>
 </template>
 
@@ -42,14 +58,23 @@ const is_selected_link = (link_label => {
    display:-webkit-box;
    display:-ms-flexbox;
    display:flex;
-   max-width:600px;
+   width:fit-content;
    margin-left:auto;
    margin-right:auto;
 }
 .nav_bar li {
-   padding:.5rem;
+   width:fit-content;
+   margin:0;
+   padding:1rem;
    padding-top:.25rem;
    padding-bottom:.25rem;
+   white-space:nowrap;
+}
+.nav_bar li:hover {
+   background:hsl(0, 0%, 96%);
+}
+.nav_bar li:not(.page_link):hover {
+   background:none;
 }
 .selected_link {
    font-weight:700;
