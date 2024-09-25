42742a40fd8e8284a2dd4f1dabeb65e709d8a2a4
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-09-25 15:41:52 +0100

    add truncate utility func

--- src/utilities/utilities/utilities.js
+++ src/utilities/utilities/utilities.js
@@ -9,3 +9,12 @@ export const scroll_to_elem = (id) => {
 export const add_if_unique_str = (arr,str) => {
    if(arr.find(elem => elem === str) === undefined) arr.push(str)
 }
+
+export const truncate = (str,len = 20,trailing = true) => {
+   if(str) {
+      if(str !== "" && str.length > len) {
+         return trailing ? str.substring(0, len) + '..' : str.substring(0, len) 
+      }
+   }
+   return str
+}
