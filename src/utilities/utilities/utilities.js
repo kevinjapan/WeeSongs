80868dfd63cb39da13a66b8a8d94751fc2296d28
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-10-04 10:57:02 +0100

    add is_subset_array

--- src/utilities/utilities/utilities.js
+++ src/utilities/utilities/utilities.js
@@ -18,3 +18,10 @@ export const truncate = (str,len = 20,trailing = true) => {
    }
    return str
 }
+
+
+// Tests if first array is a subset (therefore first is fully matched) of the second.
+// 1-d arrays of primitives only. Using .every() method removes any dependancy on order.
+export const is_subset_array = (potential_subset_arr,main_arr) => {
+   return potential_subset_arr.every(elem => main_arr.includes(elem))
+}
