6e4cbd06ee08e120f0ddbb97aa0b678b80dab948
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2025-07-10 12:23:31 +0100

    Create FormField.vue

---
+++ src/components/Form/FormField/FormField.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+
+</script>
+
+
+<template>
+
+      <section>
+
+         <slot />
+
+      </section>
+
+</template>
+
+
+<style scoped>
+
+section {
+   text-align:left;
+   display:grid;
+   grid-template-columns: 1fr 3fr;
+   align-items:center;
+}
+label {
+   color:hsl(0, 0%, 50%);
+   margin-top:2rem;
+   margin:0;
+   padding:0;
+}
+
+
+</style>
+
+
+
